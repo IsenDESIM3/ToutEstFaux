98895cb651a6f8f2ddf81e6df05799c6903f9a84
Evan Mallevaey <evan.mallevaey@isen.yncrea.fr>
2024-10-18 21:58:23 +0200

    Initial Commit

---
+++ Source/ToutEstFaux/Private/TeFGameInstance.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ToutEstFaux/Public/TeFGameInstance.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+
+UTeFGameInstance::UTeFGameInstance()
+{
+	
+}
+
+void UTeFGameInstance::Init()
+{
+	Super::Init();
+}
+
