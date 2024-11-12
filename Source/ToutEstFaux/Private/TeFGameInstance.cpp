d35c6a8353a7d993e663f25514c403ce2b95f7a7
MathRomero <romeromathias74@gmail.com>
2024-11-12 15:08:12 +0100

    [FEATURE] Multiplayer

--- Source/ToutEstFaux/Private/TeFGameInstance.cpp
+++ Source/ToutEstFaux/Private/TeFGameInstance.cpp
@@ -2,16 +2,139 @@
 
 
 #include "ToutEstFaux/Public/TeFGameInstance.h"
+#include "Engine/World.h"
 #include "OnlineSubsystem.h"
+#include "Kismet/GameplayStatics.h"
 #include "OnlineSessionSettings.h"
+#include "Online/OnlineSessionNames.h"
 
 UTeFGameInstance::UTeFGameInstance()
 {
-	
+	MySessionName = FName("MySessionName");
 }
 
 void UTeFGameInstance::Init()
 {
 	Super::Init();
+
+	IOnlineSubsystem* SubSystem = IOnlineSubsystem::Get();
+	if (SubSystem)
+	{
+		SessionInterface = SubSystem->GetSessionInterface();
+		if (SessionInterface.IsValid())
+		{
+			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UTeFGameInstance::OnCreateSessionComplete);
+			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UTeFGameInstance::OnFindSessionsComplete);
+			SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UTeFGameInstance::OnJoinSessionComplete);
+		}
+	}
 }
 
+void UTeFGameInstance::OnCreateSessionComplete(FName InSessionName, bool Succeeded)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnCreateSessionComplete: %s, Succeeded: %d"), *InSessionName.ToString(), Succeeded);
+	if (Succeeded)
+	{
+		
+		GetWorld()->ServerTravel("/Game/Assets/Multijoueur/L_Multi?listen");
+	}
+}
+
+void UTeFGameInstance::OnFindSessionsComplete(bool Succeeded)
+{
+	SearchingForServer.Broadcast(false);
+	UE_LOG(LogTemp, Warning, TEXT("OnFindSessionsComplete, Succeeded: %d"), Succeeded);
+	if (Succeeded && SessionSearch.IsValid())
+	{
+	
+		UE_LOG(LogTemp, Warning, TEXT("SearchResults, Server Count: %d"), SessionSearch->SearchResults.Num());
+		int8 ArrayIndex = -1;
+	
+		for (FOnlineSessionSearchResult Result : SessionSearch->SearchResults)
+		{
+			++ArrayIndex;
+			if (!Result.IsValid())
+				continue;
+			
+			FServerInfo Info;
+			FString ServerName = "EMPTY SERVER NMAE";
+			FString HostName = "EMPTY SERVER NMAE";
+			Result.Session.SessionSettings.Get(FName("SERVER_NAME_KEY"), ServerName);
+			Result.Session.SessionSettings.Get(FName("SERVER_HOSTNAME_KEY"), HostName);
+			Info.ServerName = ServerName;
+			Info.MaxPlayers = Result.Session.SessionSettings.NumPublicConnections;
+			Info.CurrentPlayers = Info.MaxPlayers - Result.Session.NumOpenPublicConnections;
+			Info.ServerArrayIndex = ArrayIndex;
+			Info.SetPlayerCount();
+			ServerListDel.Broadcast(Info);
+			
+	    }
+
+	}
+	
+}
+
+void UTeFGameInstance::OnJoinSessionComplete(FName InSessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnJoinSessionComplete: %s"), *InSessionName.ToString());
+
+	if (APlayerController* PController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+	{
+		FString ConnectString = "";
+		SessionInterface->GetResolvedConnectString(InSessionName, ConnectString);
+		if (ConnectString !="")
+			PController->ClientTravel(ConnectString, ETravelType::TRAVEL_Absolute);
+		
+	}
+}
+
+void UTeFGameInstance::CreateServer(FString ServerName, FString HostName)
+{
+	if (SessionInterface.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Creating Server..."));
+
+		FOnlineSessionSettings SessionSettings;
+		SessionSettings.bIsLANMatch = true;  
+		SessionSettings.bUsesPresence = true;
+		SessionSettings.NumPublicConnections = 5;
+		SessionSettings.bShouldAdvertise = true;
+		SessionSettings.bAllowJoinInProgress = true;
+		SessionSettings.bIsDedicated = false;
+
+		SessionSettings.Set(FName("SERVER_NAME_KEY"), ServerName, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
+		SessionSettings.Set(FName("SERVER_HOSTNAME_KEY"), HostName, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
+		SessionInterface->CreateSession(0, MySessionName, SessionSettings);
+	}
+}
+
+void UTeFGameInstance::FindServer()
+{
+	SearchingForServer.Broadcast(true);
+	if (SessionInterface.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("FindServer"));
+
+		SessionSearch = MakeShareable(new FOnlineSessionSearch());
+		SessionSearch->bIsLanQuery = true;
+		SessionSearch->MaxSearchResults = 10000;
+		SessionSearch->TimeoutInSeconds = 60;
+		SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+
+		SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+	}
+}
+
+void UTeFGameInstance::JoinServer(int32 ArrayIndex)
+{
+  FOnlineSessionSearchResult Result = SessionSearch->SearchResults[ArrayIndex];
+  if (Result.IsValid())
+  {
+	  UE_LOG(LogTemp, Warning, TEXT("Joining server at index: %d"), ArrayIndex);
+	  SessionInterface->JoinSession(0, MySessionName, Result);
+  }
+  else
+  {
+	  UE_LOG(LogTemp, Warning, TEXT("Failed to join server at index: %d"), ArrayIndex);
+  }
+}
