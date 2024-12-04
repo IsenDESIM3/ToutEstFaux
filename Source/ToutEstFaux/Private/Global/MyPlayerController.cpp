bb6fb6f0daf7373cf358febecce28ecd4c3d2b8e
Evan Mallevaey <evan.mallevaey@isen.yncrea.fr>
2024-12-04 11:30:36 +0100

    [CHANGE] Change something idk

--- Source/ToutEstFaux/Private/Global/MyPlayerController.cpp
+++ Source/ToutEstFaux/Private/Global/MyPlayerController.cpp
@@ -215,7 +215,7 @@ void AMyPlayerController::PutDown()
 					Target->Interact();
 					bHandEmpty = true;
 					selected->Release(FVector(0,0,0));
-				
+					
 					selected = nullptr;
 					ItemTarget = nullptr;
 				}
